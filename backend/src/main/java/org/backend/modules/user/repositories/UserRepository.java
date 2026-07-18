f67d77a010edb3b2c90f213f258bba75837173ef
Emy-gold <imanelgarate@gmail.com>
2026-07-18 20:16:45 +0100

    add the app settings for jwt

---
+++ backend/src/main/java/org/backend/modules/user/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package org.backend.modules.user.repositories;
+
+import org.backend.domains.user.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ backend/src/main/java/org/backend/security/services/JwtService.java
@@ -0,0 +1,7 @@
+package org.backend.security.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+}
