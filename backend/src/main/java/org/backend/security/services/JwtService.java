275ff57d2af98dbdf78303da27795204a3e2b8e4
Emy-gold <imanelgarate@gmail.com>
2026-07-19 20:53:56 +0100

    finis the service

--- backend/src/main/java/org/backend/security/services/JwtService.java
+++ backend/src/main/java/org/backend/security/services/JwtService.java
@@ -5,12 +5,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,7 +22,7 @@ public class JwtService {
     @Value("${app.security.jwt-secret}")
     private String secretKey;
     @Value("${app.security.jwt-expiration}")
-    private String secretException;
+    private long secretExpiration;
 
     //Extract any type of claim
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
@@ -33,6 +35,38 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
+    //generate the token in case of identifying the user
+    public String generateToken(UserDetails userDetails){
+        return  generateToken(new HashMap<>(), userDetails);
+    }
+
+
+    //generate the token in case of extra infos
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return buildToken(extraClaims, userDetails, secretExpiration);
+    }
+
+    //build the token
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            Long expiration
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    //Verify if the token still valid
+    public boolean isTokenValid(String token,UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
     //Verify if the token is expired
     private boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
@@ -41,6 +75,11 @@ public class JwtService {
     //Extract the expiration
     private Date extractExpiration(String token){
         return extractClaim(token, Claims::getExpiration);
+    }
+
+    //Get expiration time
+    public long getExpirationTime(){
+        return secretExpiration;
     }
 
     //Extract the claim from the token
