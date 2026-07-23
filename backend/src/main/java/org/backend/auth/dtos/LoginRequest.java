224c5f59ee991d6962e1bfac8f527382af056bcb
Emy-gold <imanelgarate@gmail.com>
2026-07-23 15:22:58 +0100

    add the dtos for the auth

---
+++ backend/src/main/java/org/backend/auth/dtos/LoginRequest.java
@@ -0,0 +1,26 @@
+package org.backend.auth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @Email(message = "the email is not correctly formatted")
+    @NotEmpty(message = "email is required")
+    @NotBlank(message = "email  is required")
+    private String email;
+    @NotEmpty(message = "password is required")
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "the password should be minimum 8 charachters" )
+    private String password;
+}
---
+++ backend/src/main/java/org/backend/auth/dtos/RegistrationRequest.java
@@ -0,0 +1,41 @@
+package org.backend.auth.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.backend.domains.user.Roles;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationRequest {
+
+    @NotEmpty(message = "firstname is required")
+    @NotBlank(message = "firstname is required")
+    private String firstName;
+    @NotEmpty(message = "lastname is required")
+    @NotBlank(message = "lastname is required")
+    private String lastName;
+    @Email(message = "the email is not correctly formatted")
+    @NotEmpty(message = "email is required")
+    @NotBlank(message = "email  is required")
+    private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotEmpty(message = "date of birth is required")
+    @NotBlank(message = "date of birth required")
+    private LocalDate birthDay;
+    @NotEmpty(message = "password is required")
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "the password should be minimum 8 charachters" )
+    private String password;
+    @NonNull
+    Roles role;
+
+}
