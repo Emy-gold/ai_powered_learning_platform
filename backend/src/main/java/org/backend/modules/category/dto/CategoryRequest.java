b14333f3eb1a96c16bbe5347024afbffb71e3b70
Emy-gold <imanelgarate@gmail.com>
2026-08-04 10:57:53 +0100

    add the category dto

---
+++ backend/src/main/java/org/backend/modules/category/dto/CategoryRequest.java
@@ -0,0 +1,21 @@
+package org.backend.modules.category.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequest {
+
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    private String icon;
+}
