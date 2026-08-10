10b5e29a6964d116867cd3253a41f147e019fb9a
Emy-gold <imanelgarate@gmail.com>
2026-08-10 12:35:35 +0100

    add the chat dto

--- backend/src/main/java/org/backend/modules/chat/dto/ChatRequest.java
+++ backend/src/main/java/org/backend/modules/chat/dto/ChatRequest.java
@@ -1,4 +1,24 @@
 package org.backend.modules.chat.dto;
 
-public class ChatRequest {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.backend.modules.user.dto.UserResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatResponse {
+
+    private Long id;
+    private String title;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean archived;
+    private List<UserResponse> participants;
 }
