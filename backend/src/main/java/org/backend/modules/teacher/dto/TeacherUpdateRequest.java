08c761d047078c781288d1386bf4c652f93c4ad8
Emy-gold <imanelgarate@gmail.com>
2026-08-13 12:17:31 +0100

    add the teacher update  request

---
+++ backend/src/main/java/org/backend/modules/teacher/dto/TeacherUpdateRequest.java
@@ -0,0 +1,39 @@
+package org.backend.modules.teacher.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class TeacherUpdateRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String speciality;
+
+    @Min(0)
+    @NotNull
+    private Integer experience;
+
+    @NotBlank
+    private String education;
+}
--- backend/src/main/java/org/backend/modules/teacher/service/TeacherService.java
+++ backend/src/main/java/org/backend/modules/teacher/service/TeacherService.java
@@ -1,4 +1,69 @@
 package org.backend.modules.teacher.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.backend.domains.profile.TeacherProfile;
+import org.backend.domains.user.User;
+import org.backend.modules.student.mapper.StudentMapper;
+import org.backend.modules.student.repository.StudentRepository;
+import org.backend.modules.teacher.dto.TeacherRequest;
+import org.backend.modules.teacher.dto.TeacherResponse;
+import org.backend.modules.teacher.mapper.TeacherMapper;
+import org.backend.modules.teacher.repository.TeacherRepository;
+import org.backend.modules.user.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TeacherService {
+
+    private final TeacherRepository teacherRepository;
+    private final TeacherMapper teacherMapper;
+    private final UserRepository userRepository;
+    private final StudentMapper studentMapper;
+    private final StudentRepository studentRepository;
+
+    //-----------------------------Save the teacher profile---------------------------
+    @Transactional
+    public TeacherResponse create(Long userId, TeacherRequest request){
+        if(teacherRepository.existsByUserId(userId)){
+            throw new RuntimeException("Student profile already exists for this user");
+        }
+            User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+            TeacherProfile teacherProfile = teacherMapper.toEntity(request);
+            teacherProfile.setUser(user);
+            return teacherMapper.toResponse(teacherRepository.save(teacherProfile));
+
+    }
+
+    //---------------------------Get the teacher by id -----------------------------------
+    public TeacherResponse getById(Long id){
+        TeacherProfile teacherProfile = teacherRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("teacher not found"));
+        return teacherMapper.toResponse(teacherProfile);
+    }
+
+    //----------------------------Get the teacher by user id -------------------------------
+    public TeacherResponse getByUserId(Long userId){
+        TeacherProfile teacherProfile = teacherRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+        return  teacherMapper.toResponse(teacherProfile);
+    }
+
+    //--------------------------Get all the teachers profiles--------------------------------
+    public List<TeacherResponse> getAll(){
+        List<TeacherResponse> teachers = new ArrayList<>();
+        for(TeacherProfile teacher : teacherRepository.findAll()){
+            teachers.add(teacherMapper.toResponse(teacher));
+        }
+        return teachers;
+    }
+
+    //------------------------Update the teacher profile ------------------------------------
+
 }
